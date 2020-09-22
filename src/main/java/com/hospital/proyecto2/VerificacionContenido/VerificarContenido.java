f2c1ce28454c63d7a03b67ed0fad983c3abf4aa8
benjamin <carlosbpac@gmail.com>
2020-09-22 02:02:48 -0600

    Generacion de exepciones del proyecto

--- src/main/java/com/hospital/proyecto2/VerificacionContenido/VerificarContenido.java
+++ src/main/java/com/hospital/proyecto2/VerificacionContenido/VerificarContenido.java
@@ -2,6 +2,7 @@
 package com.hospital.proyecto2.VerificacionContenido;
 
 import com.hospital.proyecto2.exepcionesDePrograma.FormatoArchivoIncorrecto;
+import com.hospital.proyecto2.exepcionesDePrograma.tipoDeSangreIncorrecto;
 import java.io.File;
 
 public class VerificarContenido {
@@ -17,6 +18,25 @@ public class VerificarContenido {
     public void archivoXML(File archivo)throws FormatoArchivoIncorrecto{
         if(!(archivo.getName().endsWith(".xml"))){
             throw new FormatoArchivoIncorrecto("El archivo de entrada no es .XML");
+        }
+    }
+    public void tipoSangre(String tipoSangre) throws tipoDeSangreIncorrecto{
+        switch(tipoSangre){
+            case "A":
+            case "A+":
+            case "A-":
+            case "B":
+            case "B+":
+            case "B-":
+            case "AB":
+            case "AB+":
+            case "AB-":
+            case "O":
+            case "O+":
+            case "O-":
+                break;
+            default :
+                throw new tipoDeSangreIncorrecto("El tipo de sangre no es correcto");
         }
     }
 }
---
+++ src/main/java/com/hospital/proyecto2/exepcionesDePrograma/tipoDeSangreIncorrecto.java
@@ -0,0 +1,12 @@
+package com.hospital.proyecto2.exepcionesDePrograma;
+
+public class tipoDeSangreIncorrecto extends Exception{
+
+    public tipoDeSangreIncorrecto() {
+    }
+
+    public tipoDeSangreIncorrecto(String message) {
+        super(message);
+    }
+    
+}
